483ce37e00b87714f3a326bd35a335209c68c91c
Josh Gordon <joshua.sam.gordon@gmail.com>
2018-02-28 00:02:23 -0500

    Make Tokenizer Work
    The tokenizer can now take a sequence of keywords, identifiers and operators, and tag them as such.

--- tokenizer.py
+++ tokenizer.py
@@ -1,25 +1,75 @@
 
 keywords = ["let","type","impure","import","from","in","macro","do","where","for","while","if","else"]
+operators = [":","=","==","+","-","*","/","%","&&","&","||","|","^","**","!","(",")","$"]
+
 
 def readFile(f):
     with open(f) as file:
         return file.read()
 
-def tokenize(s,mode):
+def tokenize(s,mode, tokenList = []):
+    if len(s) == 0:
+        return tokenList
+    #print(s,tokenList)
+    def grabAlphanumeric(s):
+        def grabAlphanumericHelper(s,done): #clobbers input until alphanumeric
+            if s[0].isalnum():
+                try:
+                    return str(s[0]) + str(grabAlphanumericHelper(s[1:],True))
+                except:
+                    return str(s[0])
+            else:
+                if not done:
+                    return grabAlphanumericHelper(s[1:],False)
+                return ""
+        return grabAlphanumericHelper(s,False)
+    def grabSymbol(s):
+        def grabSymbolHelper(s): #clobbers input until symbol
+            if len(s) == 0:
+                return None
+            if s[0] in operators:
+                if s[:2] in operators:
+                    return s[:2]
+                else:
+                    return s[0]
+            else:
+                return grabSymbolHelper(s[1:])
+        return grabSymbolHelper(s)
     
-    def grabIdentifier(s):
-        if s[0].isalnum():
-            return str(s[0]) + str(grabIdentifier(s[1:]))
-        else:
-            return ""
     if mode == "identifier":
-        token = grabIdentifier(s)
+        token = grabAlphanumeric(s)
         if token in keywords:
             return (token, "keyword")
         else:
             return (token, "identifier")
-
-result = tokenize("x = 5","identifier")
+    if mode == "operator":
+        token = grabSymbol(s)
+        return (token, "operator")
+    if mode == "all":
+        ls = tokenList
+        tokenA = grabAlphanumeric(s)
+        tokenO = grabSymbol(s)
+        #print("tokenA = ",tokenA)
+        #print("tokenO = ",tokenO)
+        trimmed = s.strip()
+        if trimmed.startswith(tokenA):
+            if tokenA in keywords:
+                ls.append((tokenA,"keyword"))
+            else:
+                ls.append((tokenA,"identifier"))
+            l = len(tokenA)
+            return tokenize(trimmed[l:],"all",tokenList=ls)
+        elif trimmed.startswith(tokenO):
+            ls.append((tokenO,"operator"))
+            l = len(tokenO)
+            return tokenize(trimmed[l:],"all",tokenList=ls)
+        else:
+            return tokenize(trimmed[1:],"all",tokenList=ls)
+        
+result = tokenize("!x = 5","all")
+        
 print(result)
+
+
     
     
