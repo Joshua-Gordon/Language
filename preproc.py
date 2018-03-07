2ecb7221d485d897abc7231e2942b73b094e9593
Josh Gordon <joshua.sam.gordon@gmail.com>
2018-03-06 22:58:31 -0500

    Add macro expanding to preprocessor

--- preproc.py
+++ preproc.py
@@ -31,11 +31,35 @@ def removeComments(code):
             lnew.append(l+";")
     return ''.join(lnew)[:-1]
 
+def expandMacros(code):
+    lines = code.split(";")
+    macros = []
+    lnew = []
+    lnew2 = []
+    for line in lines:
+        if line.strip()[0:5] == "macro":
+            idx = line.find("=")
+            macro = line[6:idx] #accounts for space after macro keyword
+            body = line[idx+1:]
+            macros.append((macro,body))
+            #print(macro,body)
+        else:
+            lnew.append(line+";")
+    for line in lnew:
+        for macro in macros:
+            line = line.replace(macro[0],macro[1])
+        lnew2.append(line)
+    return ''.join(lnew2)[:-1]
+
 def preproc(code):
     code = processNewlines(code)
+    #print("STAGE 1",code)
     code = removeComments(code)
+    #print("STAGE 2",code)
+    code = expandMacros(code)
+    #print("STAGE 3",code)
     return code
 
 
-s = "Test\nHello World\nHi//This is Josh\nThis is/* a test of*/ the preprocessor"
+s = "macro Hello=Fuck the entire\nTest\nHello World\nHi//This is Josh\nThis is/* a test of*/ the preprocessor"
 print(preproc(s))
